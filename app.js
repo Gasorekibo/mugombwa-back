77f00aff39a40b6f535584b9c4739ab45b1568fb
Gasorekibo <m.gasore@alustudent.com>
2025-06-20 09:35:14 +0200

    add body parsing middleware

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // Connect to MongoDB
 connectDB();
 
